95ae418030eed386b27e89100972bfbc47d3a3a8
anindyoo <anindyo0216@gmail.com>
2024-11-10 17:00:38 +0700

    Initial commit.

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+ 
+const element = <h1>BUCKIIN', LIKE A MECHANICAL BULL.</h1>;
+ 
+const root = createRoot(document.getElementById('root'));
+root.render(element);
