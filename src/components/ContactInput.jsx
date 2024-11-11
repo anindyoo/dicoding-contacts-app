f0a461a7cfb8d37e9a7430f8587f9d2000516af4
anindyoo <anindyo0216@gmail.com>
2024-11-11 14:48:34 +0700

    feat: implement add contact feature.

---
+++ src/components/ContactInput.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+class ContactInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      tag: '',
+    };
+    this.onInputChangeEventHandler = this.onInputChangeEventHandler.bind(this);
+    this.onSubmitEventHandler = this.onSubmitEventHandler.bind(this);
+  }
+
+  onInputChangeEventHandler(event) {
+    const { name, value } = event.target;
+    this.setState((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  }
+
+  onSubmitEventHandler(event) {
+    event.preventDefault();
+    this.props.addContact(this.state);
+  }
+
+  render() {
+    return (
+      <form className="contact-input" onSubmit={this.onSubmitEventHandler}>
+        <input          
+          type="text"          
+          name="name"          
+          placeholder="Name"
+          value={this.state.name}
+          onChange={this.onInputChangeEventHandler}
+        />
+        <input          
+          type="text"          
+          name="tag"          
+          placeholder="Tag"
+          value={this.state.tag}
+          onChange={this.onInputChangeEventHandler}
+        />
+        <button type="submit" title="Add contact button.">Add</button>
+      </form>
+    );
+  }
+}
+
+export default ContactInput;
