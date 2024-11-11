26431687f4c7e857f90ee2168390ed0e05108249
anindyoo <anindyo0216@gmail.com>
2024-11-11 13:04:10 +0700

    feat: implement delete contact feature.

--- src/components/ContactItem.jsx
+++ src/components/ContactItem.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ContactItemImage from './ContactItemImage';
 import ContactItemBody from './ContactItemBody';
+import DeleteButton from './DeleteButton';
 
-const ContactItem = ({ imageUrl, name, tag }) => (
+const ContactItem = ({ imageUrl, name, tag, id, onDelete }) => (
   <div className="contact-item">
     <ContactItemImage imageUrl={imageUrl} name={name} />
     <ContactItemBody name={name} tag={tag} />
+    <DeleteButton id={id} onDelete={onDelete} />
   </div>
 );
 
