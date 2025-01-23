9854553108c6e896030c505c861588fcbfee93f0
anindyoo <anindyo0216@gmail.com>
2025-01-23 16:05:18 +0700

    feat: implement logout feature.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,15 +1,23 @@
-import { FiHome, FiPlusCircle } from "react-icons/fi";
+import PropTypes from 'prop-types';
+import React from 'react';
+import { FiHome, FiPlusCircle, FiLogOut } from 'react-icons/fi';
 import { Link } from "react-router-dom";
 
-const Navigation = () => {
+function Navigation({ logout, name }) {
   return (
     <nav className="navigation">
       <ul>
         <li><Link to="/"><FiHome /></Link></li>
         <li><Link to="/add"><FiPlusCircle /></Link></li>
+        <li><button onClick={logout}>{name} <FiLogOut /></button></li>
       </ul>
     </nav>
   );
+}
+ 
+Navigation.propTypes = {
+  logout: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
 };
 
 export default Navigation;
